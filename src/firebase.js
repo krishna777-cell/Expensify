bc5d3fffc64b5953331fba08b07cda1e1ecfc24b
Krishna Karpe <69415586+krishna777-cell@users.noreply.github.com>
2022-07-10 18:28:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBE5k-hLjTvobaApN7BBaA2kzxapSG76SI",
-    authDomain: "expense-mang-84e64.firebaseapp.com",
-    projectId: "expense-mang-84e64",
-    storageBucket: "expense-mang-84e64.appspot.com",
-    messagingSenderId: "833355382157",
-    appId: "1:833355382157:web:4b76def65df9ec793411c4",
-    measurementId: "G-0SQ5Z21JME"
+    apiKey: "AIzaSyChIU6z_bnJJUx2opuE1UleZBAV8dnZpb4",
+  authDomain: "authenticaton-check.firebaseapp.com",
+  projectId: "authenticaton-check",
+  storageBucket: "authenticaton-check.appspot.com",
+  messagingSenderId: "414925628400",
+  appId: "1:414925628400:web:aee3d87881a5be6d303335",
+  measurementId: "G-K7KZ9V6CHJ"
 };
 
 // Initialize Firebase
